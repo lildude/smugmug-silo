b46c614ec21e4062aff411b396dd71796389341d
Colin Seymour <lildood@gmail.com>
2009-02-10 18:36:18 +0000

    Added Apache boilerplate text

--- smugmugsilo.plugin.php
+++ smugmugsilo.plugin.php
@@ -1,9 +1,26 @@
 <?php
 /**
-* SmugMug Silo
-*
-*/
+ *
+ * Copyright 2009 Colin Seymour - http://phpsmug.com/smugmug-silo-plugin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
+/**
+ * SmugMug Silo
+ *
+ */
 
 class SmugMugSilo extends Plugin implements MediaSilo
 {
@@ -21,7 +38,7 @@ class SmugMugSilo extends Plugin implements MediaSilo
 	{
 		return array('name' => 'SmugMug Media Silo',
 			'version' => '0.1',
-			'url' => 'http://phpsmug.com/smugmug-silo-plugin/',
+			'url' => 'http://phpsmug.com/smugmug-silo-plugin',
 			'author' => 'Colin Seymour',
 			'authorurl' => 'http://www.colinseymour.co.uk/',
 			'license' => 'Apache License 2.0',
