f3060ac1aae5e75b9a7dc65d60ff19a95a641ebd
Colin Seymour <lildood@gmail.com>
2009-10-09 16:12:04 +0100

    Fixed smugmugsilo_text.php

--- lib/formcontrols/smugmugsilo_text.php
+++ lib/formcontrols/smugmugsilo_text.php
@@ -1,5 +1,5 @@
 <div<?php echo ($class) ? ' class="' . $class . '"' : ''?><?php echo ($id) ? ' id="' . $id . '"' : ''?> style="clear:both">
 	<span class="pct25"><label for="<?php echo $id; ?>"><?php echo $caption; ?></label></span>
-    <span class="pct75"><input type="text" name="<?php echo $field; ?>" <?php if(!empty($style)) { echo "style='$style'"; } ?> value="<?php echo $value; ?>" <?php echo isset($disabled) ? 'disabled="disabled"' : ''; ?> <?php echo isset($tabindex) ? ' tabindex="' . $tabindex . '"' : ''?>><?php if (is_string($hpct)) {?></span><?php } ?>
+    <span class="pct75"><input type="text" name="<?php echo $field; ?>" <?php if(!empty($style)) { echo "style='$style'"; } ?> value="<?php echo $value; ?>" <?php echo isset($disabled) ? 'disabled="disabled"' : ''; ?> <?php echo isset($tabindex) ? ' tabindex="' . $tabindex . '"' : ''?>></span>
 </div>
 
